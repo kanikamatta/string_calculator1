c2c73fa3d83f3a7e9e9f7e936fdddeeb9d223b9f
Kanika <kanika.matta23@gmail.com>
2024-07-08 22:01:05 +0530

    Implemented add method to handle an empty string input and return 0

---
+++ app/models/string_calculator.rb
@@ -0,0 +1,7 @@
+# app/models/string_calculator.rb
+
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.blank?
+  end
+end
---
+++ spec/models/string_calculator_spec.rb
@@ -0,0 +1,11 @@
+# spec/models/string_calculator_spec.rb
+
+require 'rails_helper'
+
+RSpec.describe StringCalculator, type: :model do
+  describe "#add" do
+    it "returns 0 for an empty string" do
+      expect(StringCalculator.add("")).to eq(0)
+    end
+  end
+end
